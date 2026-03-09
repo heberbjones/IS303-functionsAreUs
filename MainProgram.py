6c4ee7b3029f295798e92307164ac4fa93f7fded
heberbjones <heberbjones@gmail.com>
2026-03-09 09:07:35 -0600

    I had to rename all the function files because I used a dash.

--- MainProgram.py
+++ MainProgram.py
@@ -6,3 +6,7 @@ It also does task five in GroupPland.md:
 5. Display the final record for a team. 
 Receive the home team data and display information.
 """
+
+import function1_display_intro
+
+sPlayerName = function1_display_intro.display_intro()
--- function1_display_intro.py
+++ function1_display_intro.py
@@ -1,4 +1,4 @@
-# Author Name
+# Heber Jones
 """
 1. Display an introduction to the game explaining 
 rules and prompt for their name and display that in 
@@ -6,3 +6,9 @@ the welcome message. Return the name to the main
 program and store it in variable so it can be used 
 throughout the program.
 """
+
+def display_intro():
+    print("--Game Intro--")
+    print("Here are the game rules:")
+    sPlayerName = input("Enter your name to begin: ")
+    return sPlayerName
